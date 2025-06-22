346110bbc4cf1165c0ddd04cedd8a6e8523a65a0
abhilash0511 <abhilashthelu05@gmail.com>
2025-06-22 16:51:58 +0530

    Add files via upload
    TIMERS

---
+++ project12.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+TaskHandle_t task1,task2;
+TimerHandle_t Timer1,Timer2;
+void TimerCallback1(TimerHandle_t xTimer1)
+{
+    printf("Timer1 Fired\n");
+    xTimerStart(Timer1,0);
+    vTaskResume(task1);
+}
+void TimerCallback2(TimerHandle_t xTimer2)
+{
+    printf("Timer2 Fired\n");
+    vTaskResume(task1);
+    xTimerStart(Timer2,0);
+}
+void Task1(void *data)
+{
+    vTaskSuspend(task1); 
+    printf("I'm in Task1\n");
+    Timer1 = xTimerCreate("Timer1",pdMS_TO_TICKS(2000),pdFALSE,NULL,TimerCallback1);
+    xTimerStart(Timer1,0);
+    vTaskDelete(task1);
+}
+void Task2(void *data)
+{
+    printf("I'm in Task2\n");
+    Timer2= xTimerCreate("Timer2",pdMS_TO_TICKS(2000),pdFALSE,NULL,TimerCallback2);
+    xTimerStart(Timer2,0);
+    vTaskDelete(task2);
+}
+void app_main(void)
+{
+    
+    xTaskCreatePinnedToCore(Task1,"task1",2048,NULL,2,&task1,1);
+    xTaskCreatePinnedToCore(Task2,"task2",2048,NULL,4,&task2,1);
+}
